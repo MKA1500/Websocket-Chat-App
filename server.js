b127f85fee83c58464adc2e89eb11a4ab273c6f8
MKA1500 <maciejkabsch@gmail.com>
2018-02-17 21:57:13 +0100

    work in progress

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var $ = require("jquery");
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
@@ -11,4 +12,15 @@ console.log('server running...');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log('connected: %s sockets connected', connections.length);
+
+  // disconnetct
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('disconnected: %s sockets connected', connections.length);
+  });
 });
