f9ab9c1376dcab78501ca04f1482407087a04ed0
MKA1500 <maciejkabsch@gmail.com>
2018-02-18 16:08:33 +0100

    basic version ready

--- server.js
+++ server.js
@@ -20,13 +20,26 @@ io.sockets.on('connection', function(socket){
 
   // disconnetct
   socket.on('disconnect', function(data){
+//    if(!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
     connections.splice(connections.indexOf(socket), 1);
     console.log('disconnected: %s sockets connected', connections.length);
   });
 
   // send message
   socket.on('send message', function(data){
-    console.log(data);
-    io.sockets.emit('new message', {msg: data});
+    io.sockets.emit('new message', {msg: data, user: socket.username});
   });
+
+  // new user
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
 });
