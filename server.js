08b7f319f7a725df54f367d90f05d309216847aa
MKA1500 <maciejkabsch@gmail.com>
2018-02-18 14:12:31 +0100

    chat functionality

--- server.js
+++ server.js
@@ -23,4 +23,10 @@ io.sockets.on('connection', function(socket){
     connections.splice(connections.indexOf(socket), 1);
     console.log('disconnected: %s sockets connected', connections.length);
   });
+
+  // send message
+  socket.on('send message', function(data){
+    console.log(data);
+    io.sockets.emit('new message', {msg: data});
+  });
 });
